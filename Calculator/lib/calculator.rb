ca1329768efa14ff591e3d82151631fd9068c04d
Anosike Osifo <osifo@dealdey.com>
2015-01-12 14:27:41 +0100

    fix indentation

--- Calculator/lib/calculator.rb
+++ Calculator/lib/calculator.rb
@@ -1,11 +1,11 @@
 class Calculator
-	def self.calculate(operand1, operator, operand2)
-		if(!operand1.to_s.match(/^\d+$/) && !operand2.to_s.match(/^\d+$/))		
-			raise "Ensure operands are both integer values"
-		elsif(!operator.match(/^[\*,\/,\+,\-]$/))
-			raise "Please specify a valid operator."
-		else
-			operand1.to_i.send(operator, operand2.to_i)
-		end
-	end
+  def self.calculate(operand1, operator, operand2)
+    if(!operand1.to_s.match(/^\d+$/) && !operand2.to_s.match(/^\d+$/))    
+      raise "Ensure operands are both integer values"
+    elsif(!operator.match(/^[\*,\/,\+,\-]$/))
+      raise "Please specify a valid operator."
+    else
+      operand1.to_i.send(operator, operand2.to_i)
+    end
+  end
 end
