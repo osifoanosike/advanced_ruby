5542115785c3c241fe6172edbb6c5dbe6671854a
Anosike Osifo <osifo@dealdey.com>
2015-01-15 13:29:15 +0100

    Update singleton method

--- SingletonMethod/bin/main.rb
+++ SingletonMethod/bin/main.rb
@@ -2,6 +2,7 @@ require_relative '../lib/string.rb'
 
 input = "enter a string" #this creates a String object instance
 
+#singleton method
 def input.get_vowels
 	vowels = []
 	self.each_char do |char|
@@ -12,7 +13,7 @@ end
 
 input.get_vowels
 
-
-
 input = "me" #another instance of String. 
 input.get_vowels #invokes method_missing coz it calls a singleton method
+
+puts "String class' singleton methods: #{ String.singleton_methods }"
--- SingletonMethod/lib/string.rb
+++ SingletonMethod/lib/string.rb
@@ -2,4 +2,11 @@ class String
 	def method_missing(method_name)
 		puts "The method \'#{method_name}\' doesn't exist."
 	end
+
+	#singleton method using class << self
+	class << self
+		def shout
+			self.upcase
+		end
+	end
 end
