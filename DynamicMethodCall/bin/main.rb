d3e92f1a17122a335d7b7de88479a27255859c26
Anosike Osifo <osifo@dealdey.com>
2015-01-22 09:25:18 +0100

    Fix dynamic method call, optimize code

--- DynamicMethodCall/bin/main.rb
+++ DynamicMethodCall/bin/main.rb
@@ -4,14 +4,16 @@ args = ""
 puts "enter a string"
 string = DerivedString.new(gets.chomp)
 
+
 puts "enter a method to call:\nExisting methods are: exclude?(search_param) and reverse"
 method_name = gets.chomp.downcase
+
 
 
 if method_name.match(/^exclude\?$/)
   puts "enter the string to search for exclusion"
   arg = gets.chomp
-  string.send("#{method_name}", arg)
+  puts string.send("#{method_name}", arg)
 else
-  string.send("#{method_name}")
+  puts string.send("#{method_name}")
 end
--- DynamicMethodCall/lib/derived_string.rb
+++ DynamicMethodCall/lib/derived_string.rb
@@ -1,10 +1,10 @@
 class DerivedString < String
   def exclude?(string)
-    puts "exclusion: #{!to_s.downcase.include?(string.to_s.downcase)? "Yes" : "No"}"
+    "exclusion: #{!downcase.include?(string.downcase)? "Yes" : "No"}"
   end
 
   def reverse 
-    puts to_s.reverse!
+    reverse!
   end
 
   def method_missing(method_name)
