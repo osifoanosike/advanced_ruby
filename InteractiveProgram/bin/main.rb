30742c00e9c76e537cdb24b1a4856d467f0a1092
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-03 17:04:52 +0100

    Modify code logic

--- InteractiveProgram/bin/main.rb
+++ InteractiveProgram/bin/main.rb
@@ -5,9 +5,11 @@ puts "Enter code (or q to quit, Hit enter to execute)"
 
 loop do
   input = gets
-  if input == "\n"
-    InteractiveProgram.new.execute(code, binding)
-    code = ""
+  if input == "\n"  
+   irbX = InteractiveProgram.new
+   bindingX = irbX.get_binding
+   irbX.execute(code, bindingX)
+   code = ""
   elsif /q/i =~ input 
     break
   else
--- InteractiveProgram/lib/interactive_program.rb
+++ InteractiveProgram/lib/interactive_program.rb
@@ -2,4 +2,8 @@ class InteractiveProgram
   def execute(code,mybinding)
     eval code, mybinding
   end
+
+  def get_binding
+  	return binding()
+  end
 end
