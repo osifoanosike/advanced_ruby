a3498277646d1e186f54ab52dc34d6c4a7cb349d
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-02-03 17:05:23 +0100

    Modify code logic

--- InteractiveProgram/bin/main.rb
+++ InteractiveProgram/bin/main.rb
@@ -6,10 +6,10 @@ puts "Enter code (or q to quit, Hit enter to execute)"
 loop do
   input = gets
   if input == "\n"  
-   irbX = InteractiveProgram.new
-   bindingX = irbX.get_binding
-   irbX.execute(code, bindingX)
-   code = ""
+    irbX = InteractiveProgram.new
+    bindingX = irbX.get_binding
+    irbX.execute(code, bindingX)
+    code = ""
   elsif /q/i =~ input 
     break
   else
